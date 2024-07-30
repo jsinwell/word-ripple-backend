284c136495b77279e055fb33b2045455d5d70ff4
Jon Sinwell <jonsinwell@Jons-Air.lan>
2024-07-29 17:47:34 -0700

    database working in prod

--- server.js
+++ server.js
@@ -143,4 +143,4 @@ app.post('/api/journey/complete', verifyToken, async (req, res) => {
   }
 });
 
-app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
