004e510f0733316352f456a55ee8905c53615c5e
Jon Sinwell <jonsinwell@Jons-Air.lan>
2024-07-29 17:08:18 -0700

    kms

--- server.js
+++ server.js
@@ -157,4 +157,4 @@ app.post('/api/journey/complete', verifyToken, async (req, res) => {
   }
 });
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
